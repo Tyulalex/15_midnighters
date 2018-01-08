63f1eba192055c99ec6376b066056c0feb9c2493
Tatiana Karabut <tyulalex@gmail.com>
2018-01-08 22:37:33 +0300

    seek_dev_nighters py

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+this program parses devman api 
+and print list of those accounts that made commits after midnight
+
+# System requirements
+required python 3.5 installed
+
+# How to install and run
+
+```
+  pip install -r requirements.txt
+```
+
+to run it: 
+on windows:
+```
+  python seek_dev_nighters.py
+```
+on linux might require 
+```
+  python3 seek_dev_nighters.py
+```
+
+Supported Systems: Windows, Unix
+python coursera.py
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,78 @@
+import requests
+import pytz
+import urllib.parse
+import time
+import datetime
+import logging
+import yaml
+from json.decoder import JSONDecodeError
+
+
+def load_config():
+    with open('config.yaml', 'r') as config_file:
+        return yaml.load(config_file)
+
+
+def get_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+def send_ok_request(url, attempts=3):
+    sleep_time_between_attempts_sec = 30
+    response = requests.get(url, verify=False)
+    if not response.ok and attempts > 0:
+        time.sleep(sleep_time_between_attempts_sec)
+        attempt = attempts - 1
+        send_ok_request(url, attempts=attempt)
+    elif not response.ok:
+        get_logger().error(
+            "status of requests {} not ok, but {}".format(
+                response.url, response.status_code))
+        get_logger().warning(
+            "skipping this"
+        )
+    else:
+        return response
+
+
+def fetch_json_data(url):
+    try:
+        response = send_ok_request(url, attempts=1)
+        return response.json()
+    except JSONDecodeError:
+        return None
+
+
 def load_attempts():
-    pages = 1
+    source_url = load_config()['source_url']
+    pages = fetch_json_data(source_url)['number_of_pages']
+    if not pages:
+        return None
     for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+        request_url = '{}{}'.format(
+            source_url, urllib.parse.urlencode(
+                {'page': page + 1}))
+        records = fetch_json_data(request_url)["records"]
+        for record in records:
+            yield record
+
 
 def get_midnighters():
-    pass
+    midnighters_list = []
+    midnight_dt = datetime.time(0, 0, 0)
+    morning_dt = datetime.time(7, 0, 0)
+    attempts_generator = load_attempts()
+    for attempt in attempts_generator:
+        local_timezone = pytz.timezone(attempt["timezone"])
+        utc_dt = pytz.utc.localize(
+            datetime.datetime.utcfromtimestamp(attempt["timestamp"]))
+        local_dt = utc_dt.astimezone(local_timezone)
+        if midnight_dt <= local_dt.time() <= morning_dt:
+            midnighters_list.append(attempt["username"])
+    return midnighters_list
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters()
+    print("midnighters are {}".format(midnighters))
