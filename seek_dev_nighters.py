2eb88c2fcd10a04a6af1d2f1408ea92cc881175b
Tatiana Karabut <tyulalex@gmail.com>
2018-01-11 00:29:39 +0300

    review fixes

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,66 +1,37 @@
 import requests
 import pytz
-import urllib.parse
-import time
 import datetime
-import yaml
-from json.decoder import JSONDecodeError
-
-
-def load_config():
-    with open('config.yaml', 'r') as config_file:
-        return yaml.load(config_file)
-
-
-def send_ok_request(url, attempts=3):
-    sleep_time_between_attempts_sec = 2
-    response = requests.get(url, verify=False)
-    if not response.ok and attempts > 0:
-        time.sleep(sleep_time_between_attempts_sec)
-        attempt = attempts - 1
-        send_ok_request(url, attempts=attempt)
-    elif not response.ok:
-        return
-    else:
-        return response
-
-
-def fetch_json_data(url):
-    try:
-        response = send_ok_request(url, attempts=1)
-        return response.json()
-    except JSONDecodeError:
-        return None
 
 
 def load_attempts():
-    source_url = load_config()['source_url']
-    pages = fetch_json_data(source_url)['number_of_pages']
-    if not pages:
-        return None
-    for page in range(pages):
-        request_url = '{}{}'.format(
-            source_url, urllib.parse.urlencode(
-                {'page': page + 1}))
-        records = fetch_json_data(request_url)["records"]
+    source_url = "https://devman.org/api/challenges/solution_attempts/"
+    page = 1
+    while True:
+        response = requests.get(source_url, data={"page": page}).json()
+        max_page = response["number_of_pages"]
+        records = response["records"]
         for record in records:
             yield record
+        page += page
+        if page > max_page:
+            break
 
 
-def get_midnighters():
-    midnighters_list = []
-    midnight_dt = datetime.time(0, 0, 0)
-    morning_dt = datetime.time(7, 0, 0)
-    attempts_generator = load_attempts()
-    for attempt in attempts_generator:
-        local_timezone = pytz.timezone(attempt["timezone"])
-        utc_dt = pytz.utc.localize(
-            datetime.datetime.utcfromtimestamp(attempt["timestamp"]))
-        local_dt = utc_dt.astimezone(local_timezone)
-        if midnight_dt <= local_dt.time() <= morning_dt:
-            midnighters_list.append(attempt["username"])
-    return midnighters_list
+def is_user_a_midnighter(record, date_range):
+    local_timezone = pytz.timezone(record["timezone"])
+    utc_dt = pytz.utc.localize(
+        datetime.datetime.utcfromtimestamp(record["timestamp"]))
+    local_dt = utc_dt.astimezone(local_timezone)
+    if date_range[0] <= local_dt.time() <= date_range[1]:
+        return True
 
 if __name__ == '__main__':
-    midnighters = get_midnighters()
-    print("midnighters are {}".format(midnighters))
+    midnight_dt = datetime.time(0, 0, 0)
+    morning_dt = datetime.time(7, 0, 0)
+    records = load_attempts()
+    midnighters = set()
+    for record in records:
+        if is_user_a_midnighter(record, [midnight_dt, morning_dt]):
+            midnighters.add(record["username"])
+    print("Users that made commits in time range between {} {} are:\n{}".format(
+        midnight_dt, morning_dt, "\n".join(midnighters)))
